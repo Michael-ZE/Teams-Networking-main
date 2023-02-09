138375b2fa152adf90c870aafcdfc36957f415e8
Bodea Mihai Cristian <bodeamc@gmail.com>
2023-02-09 21:42:57 +0200

    create and reload

--- index.js
+++ index.js
@@ -25,3 +25,34 @@ function displayTeams(teams) {
   document.querySelector("#teams tbody").innerHTML = teamsHTML.join("");
   console.info("display", teamsHTML, teams);
 }
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  fetch("http://localhost:3000/teams-json/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      promotion: document.getElementById("promotion").value,
+      members: document.getElementById("members").value,
+      name: document.getElementById("name").value,
+      url: document.getElementById("url").value,
+    }),
+  })
+    .then((r) => r.json())
+    .then((status) => {
+      console.warn("status", status.success, status.id);
+      if (status.success) {
+        window.location.reload();
+      }
+    });
+}
+
+function initEvents() {
+  const form = document.getElementById("editForm");
+  form.addEventListener("submit", onSubmit);
+}
+
+initEvents();
