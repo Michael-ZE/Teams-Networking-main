b03f38885970fc54e075e84d345d4ff3ca10fb88
Bodea Mihai Cristian <bodeamc@gmail.com>
2023-03-09 19:34:13 +0200

    prepare edit flow

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Teams-Networking
+
+TODO
+
+## How to deploy on github.com
+
+- `npm run build`
+- `git add . && git commit -m "new build" && git push`
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const { delay } = require("lodash");
+let allTeams = [];
+let editId;
 
 fetch("http://localhost:3000/teams-json", {
   method: "GET",
@@ -8,6 +9,9 @@ fetch("http://localhost:3000/teams-json", {
 })
   .then((r) => r.json())
   .then((teams) => {
+    //window.teams = teams;
+    allTeams = teams;
+    console.info(teams);
     displayTeams(teams);
   });
 
@@ -22,78 +26,81 @@ function createTeamRequest() {
       members: document.getElementById("members").value,
       name: document.getElementById("name").value,
       url: document.getElementById("url").value,
-    }).then((r) => r.json()),
-  });
+    }),
+  }).then((r) => r.json());
 }
 
 function deleteTeamRequest(id) {
-  fetch("http://localhost:3000/teams-json/delete", {
+  return fetch("http://localhost:3000/teams-json/delete", {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ id }),
   }).then((r) => r.json());
+}
 
-  function displayTeams(teams) {
-    console.info("display", teams);
-    const teamsHTML = teams.map(
-      (team) => `
-        <tr>
-          <td>${team.promotion}</td>
-          <td>${team.members}</td>
-          <td>${team.name}</td>
-          <td>${team.url}</td>
-          <td>
-          <a data-id = "${team.id}">❌</a>
-          </td>
-        </tr>`
-    );
+function displayTeams(teams) {
+  const teamsHTML = teams.map(
+    (team) => `
+      <tr>
+        <td>${team.promotion}</td>
+        <td>${team.members}</td>
+        <td>${team.name}</td>
+        <td>${team.url}</td>
+        <td>
+          <a data-id="${team.id}" class="remove-btn">✖</a>
+          <a data-id="${team.id}" class="edit-btn">&#9998;</a>
+        </td>
+      </tr>`
+  );
 
-    document.querySelector("#teams tbody").innerHTML = teamsHTML.join("");
-    console.info("display", teamsHTML, teams);
+  document.querySelector("#teams tbody").innerHTML = teamsHTML.join("");
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  if (editId) {
+    console.warn("update", editId);
+  } else {
+    console.warn("save");
+    createTeamRequest().then((status) => {
+      if (status.success) {
+        window.location.reload();
+      }
+    });
   }
+}
 
-  function onSubmit(e) {
-    e.preventDefault();
+function edit(id) {
+  const team = allTeams.find((team) => team.id === id);
+  console.warn("edit", id, team);
+  editId = id;
 
-    fetch("http://localhost:3000/teams-json/create", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        promotion: document.getElementById("promotion").value,
-        members: document.getElementById("members").value,
-        name: document.getElementById("name").value,
-        url: document.getElementById("url").value,
-      }),
-    })
-      .then((r) => r.json())
-      .then((status) => {
-        console.warn("status", status.success, status.id);
+  document.getElementById("promotion").value = team.promotion;
+  document.getElementById("members").value = team.members;
+  document.getElementById("name").value = team.name;
+  document.getElementById("url").value = team.url;
+}
+
+function initEvents() {
+  const form = document.getElementById("editForm");
+  form.addEventListener("submit", onSubmit);
+
+  document.querySelector("#teams tbody").addEventListener("click", (e) => {
+    if (e.target.matches("a.remove-btn")) {
+      const id = e.target.dataset.id;
+      deleteTeamRequest(id).then((status) => {
         if (status.success) {
           window.location.reload();
         }
       });
-  }
-
-  function initEvents() {
-    const form = document.getElementById("editForm");
-    form.addEventListener("submit", onSubmit);
-
-    document.querySelector("#teams tbody").addEventListener("click", (e) => {
-      if (e.target.matches("a")) {
-        const id = e.target.dataset.id;
-        deleteTeamRequest(id).then((status) => {
-          if (status.success) {
-            window.location.reload();
-          }
-          window.location.reload();
-        });
-      }
-    });
-  }
+    } else if (e.target.matches("a.edit-btn")) {
+      const id = e.target.dataset.id;
+      edit(id);
+    }
+  });
 }
 
 initEvents();
